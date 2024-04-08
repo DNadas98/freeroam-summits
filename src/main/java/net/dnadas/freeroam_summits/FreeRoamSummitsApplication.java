fdd14f6ba090aac625aef0f88eced274be726eb4
DNadas98 <daniel.nadas.123@gmail.com>
2024-04-08 12:36:27 +0200

    add initial project setup for FreeRoam Summits service

--- src/main/java/net/dnadas/freeroam_summits/FreeRoamSummitsApplication.java
+++ src/main/java/net/dnadas/freeroam_summits/FreeRoamSummitsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SummitsApplication {
+public class FreeRoamSummitsApplication {
   public static void main(String[] args) {
-    SpringApplication.run(SummitsApplication.class, args);
+    SpringApplication.run(FreeRoamSummitsApplication.class, args);
   }
 }
